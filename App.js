179f78294f508133da24b26e83d3472e5c3c5c61
Aji Muhammad Fauji <47517952+ajibon153@users.noreply.github.com>
2023-02-03 20:53:18 +0700

    leson 22

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { DevSettings } from "react-native";
 
 export default function App() {
+  useEffect(() => {
+    // if (__DEV__) {
+    //   DevSettings._nativeModule.setHotLoadingEnabled(false);
+    // }
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your goal..." />
+        <Button title="add" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List Goals...</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +24,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    marginBottom: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 3,
   },
 });
